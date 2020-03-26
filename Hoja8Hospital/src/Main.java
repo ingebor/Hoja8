c7ec99496b4956def0714ce90b9b684338922f12
IngeborRV <47764062+ingebor@users.noreply.github.com>
2020-03-26 11:52:58 -0600

    main

---
+++ Hoja8Hospital/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Ingebor Rubio 19003
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
